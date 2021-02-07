34a9238a55691d2d15badcb5e6bba1aa48b0c950
Thomas Rager <tomrager@yahoo.com>
2021-02-07 16:54:14 -0500

    completed navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
+import Me from '../img/ThomasRager.JPG'
+
+const Navigation = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar className="navbar" color="warning" light expand="md">
+        <NavbarBrand href="/"><img src={Me} alt="Thomas Rager" className="rounded-circle" width="75" height="75"/>Thomas Rager</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">Resume</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">Interests</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">Portfolio</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText className="font-weight-bolder">Full Stack Developer</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Navigation;
+
+{/*  */}
